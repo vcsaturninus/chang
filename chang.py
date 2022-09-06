901c462fe2cfe490c64960b3b84bf86f49d5ef7d
vcsaturninus <vcsaturninus@protonmail.com>
2022-09-06 08:59:49 +0100

    feat: allow specification of arbitrary number of filters and repo names
     * add new matches() utility function to quickly determine if string
       passes all filters
     * flesh out colorize() function

--- chang.py
+++ chang.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
+"""
+Copyright (c) 2022, vcsaturninus -- vcsaturninus@protonmail.com
+See https://github.com/vcsaturninus/chang
+"""
+
 import argparse
 import os
-import shutil                  # remote dir tree
+import shutil         # rm dir tree
 import sys
 import subprocess
 import re
@@ -11,8 +16,29 @@ WORKDIR="._repos/"
 PWD=os.getcwd()
 QUIET=False
 
-def colorize(s):
-    return f"\033[96m {s} \033[0m"
+def colorize(s, color=None):
+    """
+    Decorate s with ASCII colors (terminal color codes).
+    If color is not specified (default), s is returned as is.
+    """
+    colors = {
+        'black': u'\u001b[30m',
+        'red': u'\u001b[31m',
+        'green': u'\u001b[32m',
+        'yellow': u'\u001b[33m',
+        'blue': u'\u001b[34m',
+        'magenta': u'\u001b[35m',
+        'cyan': u'\u001b[36m',
+        'white': u'\u001b[37m'
+        }
+    reset = u'\u001b[0m'
+
+    color = colors[color]
+    if not color:
+        return s                          # nothing, undecorated
+    else:
+        return f"{color}{s}{reset}"       # decorate, colorize
+
 
 def log(decider, msg):
     """
@@ -24,28 +50,51 @@ def log(decider, msg):
 
 def rmdir(path, recreate=True):
     """
-    Remove PATH recursively. PATH must exist and be either a relative path 
+    Remove PATH recursively. PATH must exist and be either a relative path
     to the current working directory or an absolute path.
     The directory removed is then recreated if recreate=True (default) in
-    which case this is equivalent to removing only the contents of the 
+    which case this is equivalent to removing only the contents of the
     directory.
     """
     path = os.path.abspath(path)
-    
+
     try:
         shutil.rmtree(path)
-    except FileNotFoundError:
+    except FileNotFoundError:        # 'rm -rf' behavior
         pass
 
     if recreate:
         os.mkdir(path)
 
 
+def matches(s, match_list=None, excl_list=None):
+    """
+    True if s matches every single pattern in match_list
+    and does NOT match ANY pattern in excl_list. Otherwise
+    False.
+
+    Both match_list and excl_list are optional. If both are None,
+    calling this function always returns True.
+    """
+    if match_list:
+        for pat in match_list:
+            if not pat.search(s):
+                return False
+
+    if excl_list:
+        for pat in excl_list:
+            if pat.search(s):
+                return False
+
+    # passsed all filters
+    return True
+
+
 class Repo:
     """
-    Used to abstract fundamental repository details into 
+    Used to abstract fundamental repository details into
     a repo object, for user-friendliness. Details may include
-    the name of the repo and the url of the repo.
+    the name and url of the repo.
     """
     def __init__(self, name, url):
         self.name    = name
@@ -71,7 +120,7 @@ class Repo:
         specified by PATH. PATH is created if it does not exist. If PATH
         does exist, it MUST be a git repo and this function will attempt
         to run `git fetch -a` inside it.
-        The user must have access rights to the specified repo such that 
+        The user must have access rights to the specified repo such that
         it can be cloned or fetched from.
         """
         git_cmd = None
@@ -88,11 +137,11 @@ class Repo:
         ret = None
 
         try:
-            ret = subprocess.run(git_cmd, 
+            ret = subprocess.run(git_cmd,
                                  text=True,
-                                 timeout=100, 
-                                 check=True, 
-                                 shell=True, 
+                                 timeout=100,
+                                 check=True,
+                                 shell=True,
                                  stdout=subprocess.PIPE,
                                  stderr=subprocess.STDOUT)
 
@@ -104,16 +153,16 @@ class Repo:
             self.path = path
 
 
-    def scrape_commits(self, start=None, end=None, match=None, exclude=None):
+    def scrape_commits(self, start=None, end=None, match_list=None, excl_list=None):
         """
         Get a list of one-line git commit messages that are between [start, end],
         where start and end are either git commits or git tags.
-        
+
         If pattern is specified, the commits not matching the pattern are filtered out.
         If exclude is specified, the commits matching the exclude pattern are
         filtered out.
         """
-        cmd = f"git log --oneline" 
+        cmd = f"git log --oneline"
         if start and not end or end and not start:
             raise(Exception("Start and end commits must both be either specified or unspecified"))
         elif start and end:
@@ -123,7 +172,7 @@ class Repo:
 
         os.chdir(self.path)
         try:
-            ret = subprocess.run(cmd, 
+            ret = subprocess.run(cmd,
                                   timeout=100,                      # 100 seconds timeout
                                   check=True,                       # throw error if cmd returns error code
                                   shell=True,
@@ -141,14 +190,8 @@ class Repo:
             for line in output.split('\n'):
                 line = line.split()
                 line = ' '.join(line[1:])
-                
-                if pat and not pat.search(line):
-                    continue
-
-                if excl and excl.search(line):
-                    continue
-
-                self.commits.append(line)
+                if matches(line, match_list, excl_list):
+                    self.commits.append(line)
 
         finally:
             os.chdir(PWD)
@@ -181,12 +224,14 @@ parser.add_argument('-f',
                      )
 
 parser.add_argument('--match',
+                     nargs='*',                 # support any number of args
                      metavar='PATTERN',
                      dest='match',
                      help='Filter out commits that do NOT match PATTERN'
                      )
 
 parser.add_argument('--exclude',
+                     nargs='*',                 # support any number of args
                      metavar='PATTERN',
                      dest='exclude',
                      help='Filter out commits that match PATTERN'
@@ -208,9 +253,10 @@ parser.add_argument('-q',
 
 parser.add_argument('-r',
                      '--repo',
+                     nargs='*',                 # support any number of params
                      metavar="REPO",
                      dest='repo',
-                     help='Only look at the specified repo, ignoring the other repos supplied via the input file'
+                     help='Only look at the specified repos, ignoring the other repos supplied via the input file'
                      )
 
 
@@ -226,12 +272,14 @@ with open(args.repos, "r") as f:
         url = line.strip()
         name = url.split('/')[-1]           # name of the repo is the last component
         name = name.replace(".git", "")     # strip off .git extension substring if present
-        
+
         # skip if single repo (-r) is specified and != current
-        if args.repo and args.repo != name:
+        if args.repo and name not in args.repo:
             continue
 
         repos.append(Repo(name, url))
+
+print(args.match, args.exclude)
 
 # remove past clones IFF starting clean
 if args.clean:
@@ -241,16 +289,16 @@ if args.clean:
 if args.quiet:
     QUIET=True
 
-# REGEX
-pat  = re.compile(args.match) if args.match else None
-excl = re.compile(args.exclude) if args.exclude else None
+# REGEX lists; the cli allows specification of arbitrary number of filters
+pat  = [re.compile(m) for m in args.match ] if args.match else None
+excl = [re.compile(m) for m in args.exclude] if args.exclude else None
 
 for repo in repos:
     log(not QUIET, f" => Getting latest {repo} from {repo.get_url()}")
     repo.clone_or_fetch(WORKDIR + repo.get_name())
-    repo.scrape_commits(start=args.start_tag, end=args.end_tag, match=pat, exclude=excl)
+    repo.scrape_commits(start=args.start_tag, end=args.end_tag, match_list=pat, excl_list=excl)
 
 for repo in repos:
     l = repo.get_commits()
     for i in l:
-        print(f"[{colorize(repo)}] {i}")
+        print(f"[{colorize(repo, 'green')}] {i}")
